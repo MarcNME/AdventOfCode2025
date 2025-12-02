ab4ac114578a00d6f9a9e76cc52588c5aaae8b8a
MarcNME <marc@enzmann.dev>
2025-12-02 13:30:35 +0100

    Day 2

--- src/Helpers.kt
+++ src/Helpers.kt
@@ -2,4 +2,13 @@ import java.io.File
 
 fun readInputAsLines(fileName: String) = File("inputs/$fileName").readLines()
 
-fun readInputAsString(fileName: String) = File("inputs/$fileName").toString()
+fun readInputAsString(fileName: String) = File("inputs/$fileName").readText()
+
+fun String.isPalindrome(): Boolean {
+    val mid = this.length / 2
+
+    val firstHalf = this.take(mid)
+    val secondHalf = this.substring(mid)
+
+    return firstHalf == secondHalf
+}
