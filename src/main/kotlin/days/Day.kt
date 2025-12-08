80032cc6e3d11aff471c1ab8772c4df4d12e1683
MarcNME <marc@enzmann.dev>
2025-12-08 16:12:46 +0100

    Day 08 partly

--- src/main/kotlin/days/Day.kt
+++ src/main/kotlin/days/Day.kt
@@ -6,6 +6,6 @@ interface Day {
     fun runDay(): List<Solution>
 
     companion object {
-        fun getAllDays() = listOf(Day01(), Day02(), Day03(), Day04(), Day05(), Day06(), Day07())
+        fun getAllDays() = listOf(Day01(), Day02(), Day03(), Day04(), Day05(), Day06(), Day07(), Day08())
     }
 }
